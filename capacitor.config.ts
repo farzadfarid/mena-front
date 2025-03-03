2d32789f183da2e260d67330a3a469e23d7263ad
morteza mohammadi tabar <m.mohammaditabar64@gmail.com>
2025-03-03 10:18:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mena',
+  webDir: 'www'
+};
+
+export default config;
